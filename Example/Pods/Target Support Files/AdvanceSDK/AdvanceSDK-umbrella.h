e9e1cb100d9197e133b7d354a1f1b5bc489777f8
CherryKing <455153666@qq.com>
2020-04-17 16:22:47 +0800

    update

--- Example/Pods/Target Support Files/AdvanceSDK/AdvanceSDK-umbrella.h
+++ Example/Pods/Target Support Files/AdvanceSDK/AdvanceSDK-umbrella.h
@@ -10,24 +10,6 @@
 #endif
 #endif
 
-#import "AdvanceBanner.h"
-#import "AdvanceFullScreenVideo.h"
-#import "AdvanceInterstitial.h"
-#import "AdvanceNativeExpress.h"
-#import "AdvanceRewardVideo.h"
-#import "AdvanceSplash.h"
-#import "AdvanceBannerDelegate.h"
-#import "AdvanceFullScreenVideoDelegate.h"
-#import "AdvanceInterstitialDelegate.h"
-#import "AdvanceNativeExpressDelegate.h"
-#import "AdvanceRewardVideoDelegate.h"
-#import "AdvanceSplashDelegate.h"
-#import "CsjBannerAdapter.h"
-#import "CsjFullScreenVideoAdapter.h"
-#import "CsjInterstitialAdapter.h"
-#import "CsjNativeExpressAdapter.h"
-#import "CsjRewardVideoAdapter.h"
-#import "CsjSplashAdapter.h"
 #import "AdvanceBaseAdspot.h"
 #import "AdvanceSDK.h"
 #import "AdvanceSdkConfig.h"
@@ -36,17 +18,6 @@
 #import "SdkSupplier.h"
 #import "AdvanceDeviceInfoUtil.h"
 #import "AdvanceLog.h"
-#import "GdtBannerAdapter.h"
-#import "GdtFullScreenVideoAdapter.h"
-#import "GdtInterstitialAdapter.h"
-#import "GdtNativeExpressAdapter.h"
-#import "GdtRewardVideoAdapter.h"
-#import "GdtSplashAdapter.h"
-#import "MercuryBannerAdapter.h"
-#import "MercuryInterstitialAdapter.h"
-#import "MercuryNativeExpressAdapter.h"
-#import "MercuryRewardVideoAdapter.h"
-#import "MercurySplashAdapter.h"
 
 FOUNDATION_EXPORT double AdvanceSDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char AdvanceSDKVersionString[];
