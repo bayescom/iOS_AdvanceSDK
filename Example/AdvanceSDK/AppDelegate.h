bd2c4f2ca0d82e575e88fc5118779a1028b50364
CherryKing <455153666@qq.com>
2020-04-24 18:28:51 +0800

    将开屏模板广告改为非模板广告

--- Example/AdvanceSDK/AppDelegate.h
+++ Example/AdvanceSDK/AppDelegate.h
@@ -5,7 +5,7 @@
 //  Created by Cheng455153666 on 02/27/2020.
 //  Copyright (c) 2020 Cheng455153666. All rights reserved.
 //
-
+// org.cocoapods.demo.AdvanceSDK-Example
 @import UIKit;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
