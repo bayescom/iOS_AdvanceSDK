435f966a78734478f7a77e7bd2b1be6037b2991c
shichuanyang <shicy@bayescom.com>
2021-04-22 17:29:53 +0800

    删除快手

--- AdvanceSDK/Core/Utils/AdvSdkConfig.m
+++ AdvanceSDK/Core/Utils/AdvSdkConfig.m
@@ -15,7 +15,7 @@
 
 @implementation AdvSdkConfig
 NSString *const AdvanceSdkAPIVersion = @"3.0";
-NSString *const AdvanceSdkVersion = @"3.2.4.1";
+NSString *const AdvanceSdkVersion = @"3.2.4.2";
 NSString *const AdvanceSdkRequestUrl = @"http://cruiser.bayescom.cn/eleven";
 NSString *const AdvanceReportDataUrl = @"http://cruiser.bayescom.cn/native";
 NSString *const SDK_ID_MERCURY =@"1";
