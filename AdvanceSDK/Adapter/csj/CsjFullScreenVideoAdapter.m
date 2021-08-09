816d5fc91ef2dbb5af429d6bc54e2fec0a6e3f33
shichuanyang <shicy@bayescom.com>
2021-08-09 19:04:40 +0800

    穿山甲全屏视频

--- AdvanceSDK/Adapter/csj/CsjFullScreenVideoAdapter.m
+++ AdvanceSDK/Adapter/csj/CsjFullScreenVideoAdapter.m
@@ -36,20 +36,20 @@
 
 - (void)loadAd {
     _csj_ad.delegate = self;
-//    NSLog(@"加载穿山甲 supplier: %@ -- %ld", _supplier, (long)_supplier.priority);
+    ADV_LEVEL_INFO_LOG(@"加载穿山甲 supplier: %@ -- %ld", _supplier, (long)_supplier.priority);
     if (_supplier.state == AdvanceSdkSupplierStateSuccess) {// 并行请求保存的状态 再次轮到该渠道加载的时候 直接show
-//        ADVLog(@"穿山甲 成功");
+        ADV_LEVEL_INFO_LOG(@"穿山甲 成功");
         if ([self.delegate respondsToSelector:@selector(advanceUnifiedViewDidLoad)]) {
             [self.delegate advanceUnifiedViewDidLoad];
         }
 //        [self showAd];
     } else if (_supplier.state == AdvanceSdkSupplierStateFailed) { //失败的话直接对外抛出回调
-//        ADVLog(@"穿山甲 失败");
+        ADV_LEVEL_INFO_LOG(@"穿山甲 失败");
         [self.adspot loadNextSupplierIfHas];
     } else if (_supplier.state == AdvanceSdkSupplierStateInPull) { // 正在请求广告时 什么都不用做等待就行
-//        ADVLog(@"穿山甲 正在加载中");
+        ADV_LEVEL_INFO_LOG(@"穿山甲 正在加载中");
     } else {
-//        ADVLog(@"穿山甲 load ad");
+        ADV_LEVEL_INFO_LOG(@"穿山甲 load ad");
         _supplier.state = AdvanceSdkSupplierStateInPull; // 从请求广告到结果确定前
         [self.csj_ad loadAdData];
     }
@@ -60,13 +60,16 @@
 }
 
 - (void)dealloc {
-    ADVLog(@"%s", __func__);
+    ADV_LEVEL_INFO_LOG(@"%s", __func__);
 }
 
 
 // MARK: ======================= BUNativeExpressFullscreenVideoAdDelegate =======================
 /// 广告预加载成功回调
 - (void)nativeExpressFullscreenVideoAdDidLoad:(BUNativeExpressFullscreenVideoAd *)fullscreenVideoAd {
+}
+
+- (void)nativeExpressFullscreenVideoAdDidDownLoadVideo:(BUNativeExpressFullscreenVideoAd *)fullscreenVideoAd {
     [self.adspot reportWithType:AdvanceSdkSupplierRepoSucceeded  supplier:_supplier error:nil];
 //    NSLog(@"穿山甲全屏视频拉取成功");
     _supplier.state = AdvanceSdkSupplierStateSuccess;
@@ -131,6 +134,15 @@
             [self.delegate advanceFullScreenVideoOnAdPlayFinish];
         }
     }
+}
+
+
+- (void)nativeExpressFullscreenVideoAdViewRenderSuccess:(BUNativeExpressFullscreenVideoAd *)rewardedVideoAd {
+    
+}
+
+- (void)nativeExpressFullscreenVideoAdDidClickSkip:(BUNativeExpressFullscreenVideoAd *)fullscreenVideoAd {
+    
 }
 
 @end
