6cea99b999287aaaf9d8f2ef6d0abd44939afc4f
shichuanyang <shicy@bayescom.com>
2021-10-18 14:56:33 +0800

    快手插屏

--- AdvanceSDK/Adapter/Kuaishou/KsInterstitialAdapter.m
+++ AdvanceSDK/Adapter/Kuaishou/KsInterstitialAdapter.m
@@ -25,8 +25,8 @@
 
 @implementation KsInterstitialAdapter
 
-- (instancetype)initWithSupplier:(AdvSupplier *)supplier adspot:(AdvanceInterstitial *)adspot {
-    if (self = [super init]) {
+- (instancetype)initWithSupplier:(AdvSupplier *)supplier adspot:(id)adspot {
+    if (self = [super initWithSupplier:supplier adspot:adspot]) {
         _adspot = adspot;
         _supplier = supplier;
         _isDidLoad = NO;
@@ -36,29 +36,37 @@
 }
 
 
+- (void)supplierStateLoad {
+    ADV_LEVEL_INFO_LOG(@"加载快手 supplier: %@", _supplier);
+    _supplier.state = AdvanceSdkSupplierStateInPull; // 从请求广告到结果确定前
+    _ks_ad.delegate = self;
+    [_ks_ad loadAdData];
+}
+
+- (void)supplierStateInPull {
+    ADV_LEVEL_INFO_LOG(@"快手加载中...");
+}
+
+- (void)supplierStateSuccess {
+    ADV_LEVEL_INFO_LOG(@"快手 成功");
+    if ([self.delegate respondsToSelector:@selector(advanceUnifiedViewDidLoad)]) {
+        [self.delegate advanceUnifiedViewDidLoad];
+    }
+    
+}
+
+- (void)supplierStateFailed {
+    ADV_LEVEL_INFO_LOG(@"快手 失败");
+    [self.adspot loadNextSupplierIfHas];
+}
+
+
 - (void)deallocAdapter {
     _ks_ad = nil;
 }
 
 - (void)loadAd {
-//    ADVLog(@"加载快手 supplier: %@ -- %ld", _supplier, (long)_supplier.priority);
-    if (_supplier.state == AdvanceSdkSupplierStateSuccess) {// 并行请求保存的状态 再次轮到该渠道加载的时候 直接show
-//        ADVLog(@"快手 成功");
-        if ([self.delegate respondsToSelector:@selector(advanceUnifiedViewDidLoad)]) {
-            [self.delegate advanceUnifiedViewDidLoad];
-        }
-//        [self showAd];
-    } else if (_supplier.state == AdvanceSdkSupplierStateFailed) { //失败的话直接对外抛出回调
-//        ADVLog(@"快手 失败");
-        [self.adspot loadNextSupplierIfHas];
-    } else if (_supplier.state == AdvanceSdkSupplierStateInPull) { // 正在请求广告时 什么都不用做等待就行
-//        ADVLog(@"快手 正在加载中");
-    } else {
-//        ADVLog(@"快手 load ad");
-        _supplier.state = AdvanceSdkSupplierStateInPull; // 从请求广告到结果确定前
-        _ks_ad.delegate = self;
-        [_ks_ad loadAdData];
-    }
+    [super loadAd];
 }
 
 - (void)showAd {
@@ -69,7 +77,7 @@
 }
 
 - (void)dealloc {
-    ADVLog(@"%s", __func__);
+
 }
 
 /**
