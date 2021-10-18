d6214e208422a9b22306428c4eda61c6618cc302
shichuanyang <shicy@bayescom.com>
2021-10-18 17:50:43 +0800

    快手全屏

--- AdvanceSDK/Adapter/Kuaishou/KsFullScreenVideoAdapter.h
+++ AdvanceSDK/Adapter/Kuaishou/KsFullScreenVideoAdapter.h
@@ -5,22 +5,15 @@
 //  Created by MS on 2021/4/23.
 //
 
+#import "AdvBaseAdPosition.h"
 #import <Foundation/Foundation.h>
 #import "AdvanceFullScreenVideoDelegate.h"
 NS_ASSUME_NONNULL_BEGIN
 @class AdvSupplier;
 @class AdvanceFullScreenVideo;
 
-@interface KsFullScreenVideoAdapter : NSObject
+@interface KsFullScreenVideoAdapter : AdvBaseAdPosition
 @property (nonatomic, weak) id<AdvanceFullScreenVideoDelegate> delegate;
-@property (nonatomic, assign) NSInteger tag;// 标记并行渠道为了找到响应的adapter
-
-- (instancetype)initWithSupplier:(AdvSupplier *)supplier adspot:(AdvanceFullScreenVideo *)adspot;
-- (void)deallocAdapter;
-
-- (void)loadAd;
-
-- (void)showAd;
 
 
 @end
