784784e8c6685a382ef00b23daf62ef97009000f
shichuanyang <shicy@bayescom.com>
2022-04-13 16:13:57 +0800

    删除有问题的 SDK

--- AdvanceSDK/Core/Utils/AdvTrackEventManager/AdvAnalytics.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "AdvAnalyticsProvider.h"
-#import "AdvAnalyticsConstants.h"
-
-@interface AdvAnalytics : NSObject
-@property (nonatomic, assign) id<AdvAnalyticsProvider> provider;
-+ (instancetype)shared;
-- (void)configure:(NSDictionary *)configurationDictionary provider:(id<AdvAnalyticsProvider>)provider;
-@end
--- AdvanceSDK/Core/Utils/AdvTrackEventManager/AdvAnalyticsConstants.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-extern NSString * const AdvAnalyticsMethodCall;
-extern NSString * const AdvAnalyticsUIControl;
-extern NSString * const AdvAnalyticsClass;
-extern NSString * const AdvAnalyticsSelector;
-extern NSString * const AdvAnalyticsDetails;
-extern NSString * const AdvAnalyticsParameters;
-extern NSString * const AdvAnalyticsShouldExecute;
-extern NSString * const AdvAnalyticsEvent;
